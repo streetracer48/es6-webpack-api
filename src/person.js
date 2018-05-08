dd4283ba38ba8e142f38d9e171f2864e50ea7d30
wasifmiraz <wasifmiraz@mail.com>
2018-05-08 20:04:10 +0600

    promise with API

--- src/person.js
+++ src/person.js
@@ -1,18 +1,47 @@
-class Person 
+fetch('https://jsonplaceholder.typicode.com/users')
+
+.then((response) => response.json())
+.then((body) =>
+ {
+     const list = body.map(data =>
+         {
+let li = document.createElement('li')
+
+let text = `Name :${data.name},Email: ${data.email}`
+
+let textNode = document.createTextNode(text)
+
+li.appendChild(textNode)
+
+return li;
+         })
+
+         list.forEach((data) => {
+document.getElementById('mylist').appendChild(data);
+
+
+         });
+
+ })
+
+.catch((err) =>console.log(err));
+
+
+/* class Person
 {
     constructor(project,name,email)
     {
         this.project = project
         this.name  = name
         this.email = email
-        
+
     }
 
-    print () 
+    print ()
     {
         console.log(`projects: ${this.project} Name :${this.name}, Email:${this.email}`)
     }
 
 }
 
-export default Person;
+export default Person; */
